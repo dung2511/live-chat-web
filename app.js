ea8180ac07a3e5cc1eb8af20551ca5b0a5423168
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-05-29 13:33:26 +0700

    update

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const root = document.getElementById("root");
+const urlFacebook = "https://www.facebook.com/";
+const urlPhone = "0987654321";
+const zalo = "0987654321";
+const contactZalo = [
+  {
+    display: "Zalo",
+    src: "./images/icon-zalo.png",
+    url: `http://zalo.me/${zalo}`,
+    classbtn: "btn_zalo_main",
+    classP: "logo_zalo_main",
+  },
+];
 const iconContact = [
   {
     display: "Facebook",
     src: "./images/icon-facebook.png",
-    url: "https:facebook.com",
+    url: `${urlFacebook}`,
     classbtn: "btn_facebook_main",
     classP: "logo_facebook_main",
   },
@@ -13,17 +25,11 @@ const iconContact = [
     classbtn: "btn_mess_main",
     classP: "logo_mess_main",
   },
-  {
-    display: "Zalo",
-    src: "./images/icon-zalo.png",
-    url: "http://zalo.me/0987654321",
-    classbtn: "btn_zalo_main",
-    classP: "logo_zalo_main",
-  },
+
   {
     display: "",
     src: "./images/icon-call.png",
-    url: "tel:0987654321",
+    url: `tel:${urlPhone}`,
     classbtn: "btn_zalo_main",
     classP: "logo_zalo_main",
   },
@@ -41,5 +47,14 @@ const contact = iconContact
   })
   .join(",")
   .replaceAll(/,/g, "");
+const contact_Zalo = contactZalo.map((item) => {
+  return ` 
+  <a href=${item.url} target="_blank" class =${item.classbtn}> 
+    <p class=${item.classP} >
+      <img src=${item.src} alt="" title=${item.display}> 
+    </p>
+  </a>`;
+});
 divPlunginsMore.innerHTML += contact;
-root.appendChild(divPlunginsMore)
+divPlunginsMore.innerHTML += contact_Zalo;
+root.appendChild(divPlunginsMore);
